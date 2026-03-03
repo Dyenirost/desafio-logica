e08de5e9c746a3e24980aeee2c8f80b1a3fa630a
Dyenirost <dyenirost@gmail.com>
2026-03-02 21:54:02 -0300

    repositorio

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log ("olá mundo")
+// Variavel
+
+let nomeHeroi = "dyeni";
+let xpHeroi = 7500;
+let nivel = "";
+
+
+// Decisão
+
+switch (true) {
+    case (xpHeroi < 1000):
+    nivel = "Ferro";
+    break;
+
+    case (xpHeroi >= 1001 && xpHeroi <= 2000):
+    nivel = "Bronze";
+    break;
+
+    case (xpHeroi >= 2001 && xpHeroi <= 5000):
+    nivel = "Prata";
+    break;
+
+    case (xpHeroi >= 5001 && xpHeroi <= 7000):
+    nivel = "Ouro";
+    break;
+
+    case (xpHeroi >= 7001 && xpHeroi <= 8000):
+    nivel = "Platina";
+    break;
+
+    case (xpHeroi >= 8001 && xpHeroi <= 9000):
+    nivel = "Ascendente";
+    break;
+
+    case (xpHeroi >= 9001 && xpHeroi <= 10000):
+    nivel = "Imortal";
+    break;
+
+    case (xpHeroi >= 10001):
+    nivel = "Radiante";
+    break;
+}
+
+console.log(`O Héroi ${nomeHeroi} está no nivel ${nivel}`);
